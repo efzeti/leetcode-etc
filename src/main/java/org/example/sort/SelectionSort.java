bde44bc5a61cf6bc44fbcc967921a135c20b584a
efzeti <00franio@gmail.com>
2023-10-12 23:08:43 +0200

    added selectionsort

---
+++ src/main/java/org/example/sort/SelectionSort.java
@@ -0,0 +1,27 @@
+package org.example.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIdx] > arr[j]) {
+                    minIdx = j;
+                }
+            }
+            swap(arr, minIdx, i);
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
